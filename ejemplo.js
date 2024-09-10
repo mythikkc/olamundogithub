37807a218982746dc051d4419fb1e2b7cd62867c
mythikkc <cardenas.benumea.didierkaleb@gmail.com>
2024-09-09 20:33:35 -0600

    agregar archivo

---
+++ ejemplo.js
@@ -0,0 +1,27 @@
+
+var vestimenta=['camisa','gorra','pantalon']
+
+function mifuncion(item,index){
+    console.log(index+":"+item)
+}
+
+vestimenta.forEach(mifuncion)
+
+let array= Array.from("1A2B3C4D")
+
+console.log(array)
+
+//every
+
+let num=[32,33,15,40]
+mifuncion1()
+function validarArray(item){
+    return item>=19
+}
+
+function mifuncion1(){
+    numeros.every(validarArray)
+}
+
+
+
